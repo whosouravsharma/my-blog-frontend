554567ca9979c24938dd200e5668aa0351c5df9f
Sourav Sharma <whosouravsharma@gmail.com>
2022-10-26 00:29:43 +0530

    26-10-2022

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,21 @@
+import {useParams} from 'react-router-dom';
+import articles from './article-content';
+import NotFoundPage from './NotFoundPage';
+
+const ArticlePage = () => {
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name ===articleId);
+
+    if(!article) {
+        return <NotFoundPage />;
+    }
+
+    return (
+        <><h1>{article.title} </h1>
+        {article.content.map(paragraph => (
+            <p key={paragraph}>{paragraph}</p>
+        ))}</>
+    );
+}
+
+export default ArticlePage;
